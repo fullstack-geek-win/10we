b6264223f71263408a824b42dcc40327581c60f1
fullstack-geek-win <134689261+fullstack-geek-win@users.noreply.github.com>
2023-07-18 13:39:43 +0530

    Javascript - variables, control flow etc
    Javascript - variables, control flow etc

---
+++ Ex_07/JS/js/controlflow.js
@@ -0,0 +1,14 @@
+// for, while, switch
+// break;
+// continue;
+
+// if
+
+let i;
+
+for (i=1;i<=10;i++) {
+    console.log(i);
+    if (i==5) {
+        break;
+    }
+}
---
+++ Ex_07/JS/js/script.js
@@ -0,0 +1,64 @@
+let i,j;
+
+// i = parseInt(prompt('Enter any number'));
+// i = prompt('Enter any number');
+
+// '10'==10 -> true
+// '10'===10 -> false
+// 10===10 -> true
+
+// if (i==10) {
+//     alert("i=10");
+// }
+// else if (i==20) {
+//     alert("i=20");
+// }
+// else if (i==30) {
+//     alert("i=20");
+// }
+// else {
+//     alert("i not either 10 or 20");
+// }
+
+i="Geetha";
+switch (i) {
+    case "Balaji":
+        alert("i=10");
+        break;
+    case "Geetha":
+        alert("i=20");
+        break;
+    default:
+        alert("i not either 10 or 20");
+}
+
+
+
+// &&
+// true && ture -> true
+
+
+
+// j=5;
+// console.log('value of i + j = ' + (i + j));
+// console.log('value of i - j = ' + (i - j));
+// console.log('value of i * j = ' + (i * j));
+// console.log('value of i / j = ' + (i / j));
+// console.log('value of i % j = ' + (i % j));
+
+
+// i++;
+i+=3;
+// alert(i);
+// i=i+1;
+console.log('value of i = ' + (i));
+console.log('value of i = ' + (++i));
+console.log('value of i = ' + (i++));
+console.log('value of i = ' + (i));
+
+
+
+// // i--;
+// i-=8;
+// // i=i-1;
+// console.log('value of i = ' + (i));
